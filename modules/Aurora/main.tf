9886c4001fa48b6a89058dbe80cc0f8990a11b97
jagadish valluri <jagadishvalluri64@gmail.com>
2026-05-04 21:05:07 +0530

    primary work for global database is complete , tested functionality

--- modules/Aurora/main.tf
+++ modules/Aurora/main.tf
@@ -8,6 +8,9 @@ resource "aws_rds_global_cluster" "application_global_cluster_rds" {
 
 resource "aws_rds_cluster" "primary_cluster" {
   #provider                  = aws.primary
+
+  port = var.port
+  cluster_scalability_type = var.cluster_scalability_type_primary
   engine                    = aws_rds_global_cluster.application_global_cluster_rds.engine
   engine_version            = aws_rds_global_cluster.application_global_cluster_rds.engine_version
   cluster_identifier        = var.primary_cluster_identifier
@@ -16,6 +19,47 @@ resource "aws_rds_cluster" "primary_cluster" {
   database_name             = "example_db"
   global_cluster_identifier = aws_rds_global_cluster.application_global_cluster_rds.id
   db_subnet_group_name      = var.db_subnet_group_name_primary
+  availability_zones = var.availability_zones_primary
+  
+  iam_database_authentication_enabled = var.iam_database_authentication_enabled_primary
+ 
+  iam_roles = var.iam_roles_primary
+  engine_mode = var.engine_mode
+
+  iops = var.iops_primary
+  monitoring_interval = var.monitoring_interval_primary
+  monitoring_role_arn  = var.monitoring_role_arn_primary
+  network_type = var.network_type_primary
+
+
+  performance_insights_enabled = var.performance_insights_enabled
+
+  database_insights_mode  = var.database_insights_mode
+
+  preferred_backup_window = var.preferred_backup_window
+  preferred_maintenance_window = var.preferred_maintenance_window
+
+
+  performance_insights_retention_period = var.performance_insights_retention_period
+  skip_final_snapshot = var.skip_final_snapshot
+
+  vpc_security_group_ids = var.vpc_security_group_ids_primary
+
+  backup_retention_period = var.backup_retention_period
+  
+  enabled_cloudwatch_logs_exports = var.enabled_cloudwatch_logs_exports
+  enable_local_write_forwarding = var.enable_local_write_forwarding
+
+  deletion_protection = var.deletion_protection
+
+ /* restore_to_point_in_time {
+    source_cluster_identifier  = aws_rds_cluster.secondary_cluster.id
+    restore_type               = "copy-on-write"
+    use_latest_restorable_time = true
+  } */
+
+  performance_insights_kms_key_id = var.performance_insights_kms_key_id_primary
+
 }
 
 resource "aws_rds_cluster_instance" "primary" {
@@ -26,16 +70,27 @@ resource "aws_rds_cluster_instance" "primary" {
   cluster_identifier   = aws_rds_cluster.primary_cluster.id
   instance_class       = var.primary_instance_class
   db_subnet_group_name = var.db_subnet_group_name_primary
+
+  auto_minor_version_upgrade = var.auto_minor_version_upgrade
+
+  custom_iam_instance_profile = var.custom_iam_instance_profile_primary
+  publicly_accessible = var.publicly_accessible
+
 }
+
 
 resource "aws_rds_cluster" "secondary_cluster" {
   provider                  = aws.secondary
   engine                    = aws_rds_global_cluster.application_global_cluster_rds.engine
+  replication_source_identifier = aws_rds_cluster.primary_cluster.id
   engine_version            = aws_rds_global_cluster.application_global_cluster_rds.engine_version
   cluster_identifier        = var.secondary_cluster_identifier
   global_cluster_identifier = aws_rds_global_cluster.application_global_cluster_rds.id
   skip_final_snapshot       = var.skip_final_snapshot
   db_subnet_group_name      = var.db_subnet_group_name_secondary
+
+  enabled_cloudwatch_logs_exports = var.enabled_cloudwatch_logs_exports
+  enable_local_write_forwarding = var.enable_local_write_forwarding
 
   lifecycle {
     ignore_changes = [
--- modules/Aurora/variables.tf
+++ modules/Aurora/variables.tf
@@ -49,7 +49,7 @@ variable "skip_final_snapshot" {
 variable "db_subnet_group_name_secondary" {
     description = "database subnet group name for secondary cluster accross region"
     type = string
-    default = "default"
+    default = "secondary"
 }
 
 
@@ -76,4 +76,155 @@ variable "secondary_instance_class" {
     description = "secondary instance type"
     type = string
     default = "db.r4.large"
+}
+
+variable "availability_zones_primary"{
+    description = "availability zones for primary data base"
+    type = list(string)
+    default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "cluster_scalability_type_primary" {
+    description = "cluster scalabilty type"
+    type = string
+    default = "standard"
+}
+
+variable "iam_database_authentication_enabled_primary"{
+    description = "iam database authentication"
+    type = bool
+    default = false
+    
+}
+
+
+variable "iam_roles_primary" {
+    description = "List of ARNs for the IAM roles to associate to the RDS Cluster"
+    type = list(string)
+    default = null
+}
+
+variable "iops_primary" {
+    description = "Amount of Provisioned IOPS (input/output operations per second) to be initially allocated for each DB instance in the Multi-AZ DB cluster"
+    type = string
+    default = null
+}
+
+variable "monitoring_interval_primary" {
+    description = "Interval, in seconds, between points when Enhanced Monitoring metrics"
+    type = number
+    default = 0
+}
+
+variable "monitoring_role_arn_primary" {
+    description = "monitoring role for the primary DB"
+    type = string
+    default = null
+}
+
+variable "network_type_primary" {
+    description = "network type for primary db"
+    type = string
+    default = "IPV4"  ##  Valid values: IPV4, DUAL
+}
+
+variable "port" {
+    description = "port for the database"
+    type = number
+    default = 6345
+}
+
+variable "performance_insights_enabled" {
+    description = "performence insights for the database"
+    type = bool
+    default = false
+}
+
+variable "preferred_backup_window"{
+    description = "Daily time range during which automated backups are created if automated backups are enabled using the BackupRetentionPeriod parameter"
+    type = string
+    default = "14:00-19:00"
+}
+
+variable "preferred_maintenance_window"{
+   description = "Weekly time range during which system maintenance can occur, in (UTC)"
+   type = string
+   default = "wed:04:00-wed:04:30"
+}
+
+
+
+
+variable "vpc_security_group_ids_primary" {
+    description = "vpc security groups to be associated"
+    type = list(string)
+    default = [""]
+}
+
+variable "database_insights_mode" {
+    description = "data base insights"
+    type = string
+    default = null
+
+}
+
+variable "backup_retention_period" {
+    description = " back up retention period in days"
+    type = number
+    default = 1
+}
+
+variable "performance_insights_kms_key_id_primary" {
+    description = "kms key id for primary db perfoirmance insight encryption"
+    type = string
+    default = null
+}
+
+
+variable "performance_insights_retention_period" {
+    description = "rentention period for performance insights"
+    type = number
+    default = 0 
+}
+
+variable "engine_mode" {
+    description = "engine mode"
+    type = string
+    default =  "provisioned"
+}
+
+variable "enable_local_write_forwarding"{
+    description = "enable local write forwarding from read"
+    type = bool
+    default= false
+}
+
+variable "enabled_cloudwatch_logs_exports"{
+    description = "enable cloud watch logs"
+    type = list(string)
+    default = null ##["general" ,"audit", "error", "instance", "postgresql"]
+}
+
+variable "auto_minor_version_upgrade" {
+    description = "Indicates that minor engine upgrades will be applied automatically to the DB instance during the maintenance window"
+    type = bool
+    default = false
+}
+
+variable "deletion_protection"{
+    description = "deletion protection for database"
+    type = bool
+    default = false
+}
+
+variable "custom_iam_instance_profile_primary" {
+    description = "custom instance profile for primary database"
+    type = string
+    default = null
+}
+
+variable "publicly_accessible" {
+    description = "public access for database"
+    type = bool
+    default = false
 }
